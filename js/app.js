0c95bf98e833f1083790602611d2d7eacde243f3
vihong <probamaths@gmail.com>
2019-08-12 13:29:06 +0200

    embellissage dans le message

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@
     var tentative = 2;
     console.log(numberToGuess);
 
-    var answer = window.prompt("Devinez notre chiffre entre 0 et 10");
+    var answer = window.prompt("Devinez notre chiffre entre 0 et 10 (bornes incluses)");
     while (answer != numberToGuess && tentative > 0)
     {        
         if (answer > numberToGuess)
-            alert(`Plus bas...\nPlus que ${tentative} tentatives`);
+            alert(`Plus bas...\nIl vous reste ${tentative} tentatives`);
         else
-            alert(`Plus haut...\nPlus que ${tentative} tentatives`);
+            alert(`Plus haut...\nIl vous reste ${tentative} tentatives`);
         tentative--;
         answer = window.prompt("Devinez notre chiffre entre 0 et 10");
     }
     if (answer == numberToGuess)
     {
-        alert(`Bravo, c'est trouvé, vous avez GAGNÉ`)
+        alert(`GAGNÉ : vous avez trouvé !!! Le chiffre à deviner était bien ${numberToGuess} !`)
         return
     }
     else
     {
-        alert(`Dommage, vous n'avez pas trouvé, vous avez PERDU`)
+        alert(`PERDU : vous n'avez pas trouvé... Le chiffre à deviner était ${numberToGuess} !`)
         return
     }
 })();
@@ -33,7 +33,7 @@ function getRandomInt(max) {
 
 /*
 
-But du jeu : 
+PSEUDO CODE
 générer un chiffre en tre 0 et 10 et demander au
 bravo
 au dessus
@@ -42,41 +42,4 @@ en dessous
 
 prompt et alert
 
-*/
-
-
-
-
-
-
-
-/*(function () {
-    const nb = getRandomInt(11);
-    let essai = 2;
-    console.log(nb);
-    let answer = window.prompt("Deviner le chiffre entre 0 et 10");
-
-    while (answer != nb && essai > 0) {
-        if (answer < nb)
-            alert("Au-dessus...");
-        else
-            alert("En-dessous...");
-        answer = window.prompt(`Deviner le chiffre entre 0 et 10. \nReste : ${essai} essai(s)`);
-        essai--;
-    }
-    if (answer == nb && essai != 0) {
-        alert("Bravo !!!");
-        return;
-    }
-    else{
-        alert(`Reste ${essai} essai.\nC'est PERDU...`);
-        return;
-    }
-
-
-})();
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-};
 */

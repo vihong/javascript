b31a5f22e06be094b33aad634e99736bdaac4c70
vihong <probamaths@gmail.com>
2019-08-14 09:05:04 +0200

    "dernière modif avec set timer mais non terminé"

--- js/app-2.js
+++ js/app-2.js
@@ -1,6 +1,11 @@
 (function(){
-    var = function(){
-        console.log("demo");
-    }
-    window.setInterval(demo, 1000);
+    
+    var i = 0;
+    var timer = window.setInterval(function(){
+        i++;
+        console.log(i);
+        if (i === 10)
+            window.clearInterval(3);
+    }, 1000);
+    console.log(timer);
 })();
